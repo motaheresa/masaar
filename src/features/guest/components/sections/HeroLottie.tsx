47796a5dc4cfc19d4866b14a8510f397d4c753bb
MOHAMED TAHER <119441809+motaheresa@users.noreply.github.com>
2025-12-14 17:49:30 +0200

    v3 completed

--- src/features/guest/components/sections/HeroLottie.tsx
+++ src/features/guest/components/sections/HeroLottie.tsx
@@ -1,20 +1,20 @@
 "use client";
 
 import Image from "next/image";
-import HeroLottieImage from "@/assets/images/student with laptop.gif";
-// import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+// import HeroLottieImage from "@/assets/images/student with laptop.gif";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export const HeroLottie = () => {
   return (
     <div className="w-full max-w-[550px] aspect-square">
-      {/* <DotLottieReact
+      <DotLottieReact
         src={"/guestHero.lottie"}
         speed={0.3}
         autoplay  
         loop
         style={{ width: "100%", height: "100%" }}
-      /> */}
-      <Image src={HeroLottieImage} alt="Hero Lottie" width={550} height={550} priority fetchPriority="high" />
+      />
+      {/* <Image src={HeroLottieImage} alt="Hero Lottie" width={550} height={550} priority fetchPriority="high" /> */}
     </div>
   );
 };
